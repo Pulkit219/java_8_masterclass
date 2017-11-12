9fa22c5a9c7f211dbe02dcca0c30745e1bacb5b0
Pulkit219 <ascension1110@gmail.com>
2017-11-11 22:55:30 -0500

    14th comm
    practicing composition

---
+++ Composition2/src/com/pulkit/composition2/Bedroom.java
@@ -0,0 +1,12 @@
+package com.pulkit.composition2;
+
+public class Bedroom {
+    private Wall wall1;
+    private Wall wall2;
+    private Wall wall3;
+    private Wall wall4;
+    private Lamp lamp;
+    private Bed bed;
+
+    
+}
